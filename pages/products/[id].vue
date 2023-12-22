54b73afac44660fcab228a8d876b41851afb9b4e
Ifazo <ziaulkarimifaz@outlook.com>
2023-12-22 21:09:44 +0600

    tailwind init and page done

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ params.id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae earum
+      quis a soluta incidunt in perferendis pariatur, voluptatum rem non
+      excepturi atque aspernatur est sapiente. Suscipit magnam alias deleniti?
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { params } = useRoute();
+</script>
+
+<style lang="scss" scoped></style>
